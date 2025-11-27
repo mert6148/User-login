d8db917ae153e3d239cf684429c54005c1dfb325
mert6148 <154323978+mert6148@users.noreply.github.com>
2025-11-27 22:01:46 +0300

    Update UWP_UserControl.xaml.cs

--- assets/UWP_UserControl.xaml.cs
+++ assets/UWP_UserControl.xaml.cs
@@ -19,7 +19,69 @@ namespace assets
     {
         public UWP_UserControl()
         {
-            this.InitializeComponent();
+            Console.WriteLine("assets/UWP_UserControl.xaml.cs loaded");
+            double width = 300;
+            double height = 200;
+            this.Width = width;
+            this.Height = height;
+
+            byte[] pixels = new byte[(int)(width * height * 4)];
+            for (int i = 0; i < pixels.Length; i += 4)
+            {
+                assets.UWP_Utils.SetPixel(pixels, i, 0, 255, 0, 255); // ARGB format for green
+                assets.UWP_Utils.SetPixel(pixels, i + 1, 255, 0, 0, 255); // ARGB format for red
+                assets.UWP_Utils.SetPixel(pixels, i + 2, 0, 0, 255, 255); // ARGB format for blue
+                assets.UWP_Utils.SetPixel(pixels, i + 3, 255, 255, 0, 255); // ARGB format for yellow
+
+                i += 16; // Skip some pixels to create a pattern
+                if (i >= pixels.Length)
+                    break;
+
+                var index = i;
+            }
+        }
+
+        public void UserControl_Loaded(object sender, RoutedEventArgs e)
+        {
+            foreach (var child in LogicalTreeHelper.GetChildren(this))
+            {
+                Console.WriteLine("Child element: " + child.GetType().ToString());
+
+                double childWidth = (child as FrameworkElement).ActualWidth;
+                double childHeight = (child as FrameworkElement).ActualHeight;
+
+                writeLine("Child dimensions: " + childWidth + "x" + childHeight);
+            }
+        }
+
+        private void writeLine(string message)
+        {
+            Console.WriteLine(message);
+
+            double width = this.ActualWidth;
+            double height = this.ActualHeight;
+            byte[] pixels = new byte[(int)(width * height * 4)];
+            byte[] textPixels = assets.UWP_Utils.RenderTextToPixels(message, (int)width, (int)height);
+
+            index = 0;
+            for (int i = 0; i < pixels.Length; i += 4)
+            {
+                if (index < textPixels.Length)
+                {
+                    byte a = textPixels[index + 3];
+                    byte r = textPixels[index + 2];
+                    byte g = textPixels[index + 1];
+                    byte b = textPixels[index + 0];
+                    assets.UWP_Utils.SetPixel(pixels, i, r, g, b, a);
+                }
+                
+                index += 4;
+                foreach (var child in LogicalTreeHelper.GetChildren(this))
+                {
+                    double childWidth = (child as FrameworkElement).ActualWidth;
+                    double childHeight = (child as FrameworkElement).ActualHeight;
+                }
+            }
         }
     }
 }
