e61089a135e52d2fb3a2923eaebf7bec10e3e413
mert6148 <154323978+mert6148@users.noreply.github.com>
2025-12-01 17:37:48 +0300

    Update index.php

--- src/index.php
+++ src/index.php
@@ -1,63 +1,28 @@
 <?php
 
-use App\Service\AdminService;
-use PHPUnit\Framework\TestCase;
-use PHPUnit\Framework\Attributes\CoversClass;
-use PHPUnit\Framework\Attributes\Group;
-use Exception;
+use App\Helpers\TemplateHelper;
+use App\Models\Template;
 
-class AdminService
+class Templates
 {
-    public function getSiteStats(): array
+    protected $templateHelper;
+
+    public function __construct(TemplateHelper $templateHelper)
     {
-        return [
-            'total_users' => 120,
-            'active_sessions' => 8,
-            'orders_today' => 24
-        ];
+        $this->templateHelper = $templateHelper;
     }
 
-    public function getUserCount(): int
+    public function getAllTemplates()
     {
-         /**
-          * @return int
-          * @throws \Exception
-          */
+        return Template::all();
+    }
 
-        if (condition) {
-            // Some code here
-            var_dump('This is a debug message');
-            $this->assertDirectoryIsReadable($directory);
+    public function renderTemplate($templateId, $data)
+    {
+        $template = Template::find($templateId);
+        if ($template) {
+            return $this->templateHelper->render($template->content, $data);
         }
-
-    }
-
-    public function getActiveSessions(): int
-    {
-        return 8;
-    }
-}
-
-class AdminServiceTest extends TestCase
-{
-    #[CoversClass(AdminService::class)]
-    #[Group('unit')]
-    public function testGetSiteStats(): void
-    {
-        $adminService = new AdminService();
-        $stats = $adminService->getSiteStats();
-
-        $this->assertIsArray($stats);
-        $this->assertArrayHasKey('total_users', $stats);
-        $this->assertArrayHasKey('active_sessions', $stats);
-        $this->assertArrayHasKey('orders_today', $stats);
-    }
-
-    #[CoversClass(AdminService::class)]
-    #[Group('unit')]
-    public function testGetUserCount(): void
-    {
-        $adminService = new AdminService();
-        $userCount = $adminService->getUserCount();
+        return null;
     }
 }
